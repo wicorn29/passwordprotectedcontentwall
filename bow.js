70f7746fe364b76854642c572b3597c3d81946d4
Wil Corn <wilson.corn@hotmail.com>
2023-03-09 09:46:39 -0800

    Create bow.js

---
+++ bow.js
@@ -0,0 +1,8 @@
+
+
+
+poop = prompt("What is the password WIl gave you?", "");
+
+if(poop == "zopersoon01"{
+            window.location.href="https://sites.google.com/view/9274657492765701045-insession"
+        alert("Valid");
